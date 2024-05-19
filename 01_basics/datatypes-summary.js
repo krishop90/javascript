afffe86ea0d2d0e18412495022d6abd7339e4f0a
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 10:25:53 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -7,13 +7,37 @@
 
 // javascripts is dynamic type of language 
 
-const heros = ["krish" , "ironman" , "thor"];
-let myObj = {
-    name: "krishh",
-    age: 19,
-}
-console.log(myObj);
-let myFunction =  function(){
-    console.log("hello world");
-}
- console.log(myFunction);
+// const heros = ["krish" , "ironman" , "thor"];
+// let myObj = {
+//     name: "krishh",
+//     age: 19,
+// }
+// console.log(myObj);
+// let myFunction =  function(){
+//     console.log("hello world");
+// }
+//  console.log(myFunction);
+
+
+ //********************************************************* */
+
+ //Stack (primitive)  Heap(non primitive)
+
+ let myName = "krish";
+ let anotehrName = myName;
+anotehrName = "krunal";
+ console.log(anotehrName);
+ console.log(myName);
+
+ let useOne = {
+    email: "user@gmail.com",
+    age : 19
+ }
+
+ let useTwo = useOne;
+useTwo.email= "krish@gmail.com";
+console.log(useOne.email);
+console.log(useTwo.email);
+
+// we got copy from stack and 
+// we got reference from heap(actual value)
