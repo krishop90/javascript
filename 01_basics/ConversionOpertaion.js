bcf9dc06e97ac5e534ada1ff7eb2b0ce6ec0c3a2
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 09:34:01 +0000

    conversion of string

--- 01_basics/ConversionOpertaion.js
+++ 01_basics/ConversionOpertaion.js
@@ -1,13 +1,13 @@
-let score = null;
+// let score = null;
 
-console.log(typeof score);
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// // console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
- let IsLoggedIn = " ";
- let BooleanIsLoggedIn = Boolean(IsLoggedIn);
- console.log(BooleanIsLoggedIn);
+//  let IsLoggedIn = " ";
+//  let BooleanIsLoggedIn = Boolean(IsLoggedIn);
+//  console.log(BooleanIsLoggedIn);
 
 
 // string to number "33" => 33
@@ -16,6 +16,37 @@ console.log(valueInNumber);
 // "" => false
 // "krish " => true
  
-let numbers = 22;
-let toString = String(numbers);
-console.log(toString);
+// let numbers = 22;
+// let toString = String(numbers);
+// console.log(toString);
+// let str = "hello";
+// let str2 = "krish";
+// console.log(str +" " + str2);
+
+// console.log("1" + 2 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * (5 % 6));
+
+
+
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"krish");
+
+
+
+// let num1,num2,num3;
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+let x = 2;
+let y = x--;
+console.log(y);
+
