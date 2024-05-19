7a5f04119c28576c0af3c50d75131865212a0281
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 10:25:06 +0000

    strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,16 @@
+const name = "krish";
+const repoCount = 10;
+console.log(name + repoCount + "value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("   bgmi   ");
+console.log(gameName);
+console.log(gameName[2]);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName);
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('m'));
+console.log(gameName.bold);
+console.log(gameName.split(0,2));
+console.log(gameName.trim());
