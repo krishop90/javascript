7950a11d0720ac3d97dc83ad98ed056293fcd256
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 11:00:09 +0000

    date and time

---
+++ 01_basics/dates.js
@@ -0,0 +1,13 @@
+// let date = new Date()
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(typeof date);
+
+// let mydate = new Date(2024,5,30);
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toDateString());
+
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
