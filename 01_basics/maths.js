a36f76a9fa94678cfc781fc0d33937fc3fdf9fd2
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 10:45:51 +0000

    maths and number

---
+++ 01_basics/maths.js
@@ -0,0 +1,30 @@
+// const score = 400;
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(score);
+// console.log(score.toString());
+
+// console.log(balance.toFixed(2));
+
+// const isNumber = 223.9004;
+// console.log(isNumber.toPrecision(3));
+
+// const thousands = 100000000;
+// console.log(thousands.toLocaleString('en-IN'));
+
+
+//********************* MATHS ************************** */
+
+// console.log(Math);
+// console.log(Math.random()*10 - 1);
+// console.log(Math.ceil(Math.random()*10));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1))+ min)
+
+
+// random generates values between 0 and 1 
+// to multiply 10 will give values between 0 to 10 in points
+// floor will give sometimes value as 0
+// to overcome it we have to plus by 1
