e1a85f5212de75a28f4d4f571a8b58ad4bcd5ce5
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 09:54:17 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// primitive 
+// 7 types : string Number Boolean null undefined Symbol BigInt
+
+
+// reference type (Non primitive)
+// Array Objects Functions
+
+// javascripts is dynamic type of language 
+
+const heros = ["krish" , "ironman" , "thor"];
+let myObj = {
+    name: "krishh",
+    age: 19,
+}
+console.log(myObj);
+let myFunction =  function(){
+    console.log("hello world");
+}
+ console.log(myFunction);
