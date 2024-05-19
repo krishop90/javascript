db8878a44f7ce1b14a3fc344bd2e486875dee7ec
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 09:15:46 +0000

    datatypes conversion

---
+++ 01_basics/ConversionOpertaion.js
@@ -0,0 +1,21 @@
+let score = null;
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+ let IsLoggedIn = " ";
+ let BooleanIsLoggedIn = Boolean(IsLoggedIn);
+ console.log(BooleanIsLoggedIn);
+
+
+// string to number "33" => 33
+// "33abc" => NaN
+// true <=> 1 and false <=> 0
+// "" => false
+// "krish " => true
+ 
+let numbers = 22;
+let toString = String(numbers);
+console.log(toString);
