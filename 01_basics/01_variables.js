173a35a4de916cd152e2d832dbb07eb8f27018a6
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-19 08:58:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345;
+let accountEmail = "krish90@gmail.com";
+var password = "90";
+accountCity = "surat";
+let accountState;
+// accountId = 2;
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountEmail, accountId , accountCity ,accountState])
