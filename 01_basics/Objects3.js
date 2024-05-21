d195b5aa2d3255b57664d5c555f221d41417445a
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-21 13:26:12 +0000

    Objects3

---
+++ 01_basics/Objects3.js
@@ -0,0 +1,25 @@
+// Objects de-structure
+
+const course = {
+    name: "Java script",
+    fee : 1000,
+    courseInstructor : "krish"
+} 
+const {courseInstructor : instructor} = course;
+console.log(instructor);
+
+//APIs objects
+
+{
+    "name" : "krish",
+    "age" : 18
+}
+
+
+
+//APIs Array
+[
+    {},
+    {},
+    {}
+]
