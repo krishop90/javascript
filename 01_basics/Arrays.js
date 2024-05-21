0dcbddcc43a8e2da6a1cb59ec25fb2b2624e3341
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-21 12:11:48 +0000

    Arrays

---
+++ 01_basics/Arrays.js
@@ -0,0 +1,41 @@
+//arrays
+
+const myArr = [0 , 1 , 2 , 3 , 4 , 5];
+// const heros = ["krish","ironman"];
+// const myArray = new Array(1,2,3,4,5);
+
+//console.log(myArr);
+
+// array method
+
+// myArr.push(6);
+// myArr.pop();
+// myArr.unshift(9);
+// const newArr = myArr.join()
+
+// console.log(typeof myArr);
+// console.log(typeof newArr);
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(5));
+
+
+
+
+//slice , splice
+
+console.log("A" , myArr);
+
+
+const myArr1 = myArr.slice(1,3);
+console.log(myArr1);
+console.log("B" , myArr);
+
+const myArr2 = myArr.splice(1,3);
+console.log(myArr2);
+console.log("C" , myArr);
+
+
+// difference between slice and splice 
+
+
+
