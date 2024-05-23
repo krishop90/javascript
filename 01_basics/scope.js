0d662afb6659c0e7c9ceb76f485d9258f1abf50b
krishop90 <124892006+krishop90@users.noreply.github.com>
2024-05-23 05:52:45 +0000

    local and global block scope

--- 01_basics/scope.js
+++ 01_basics/scope.js
@@ -8,6 +8,49 @@ if(true){
     var b =200;
 }
 
+ // {}  <- is a scope  
+
 //console.log(a); // undefined 
 //console.log(c); // undefined
-console.log(b); // printed
+//console.log(b); // printed
+
+
+function one (){
+    const name = "krish";
+
+
+    function two(){
+        const site = " youtube"
+        console.log(name);
+    }
+    //console.log(site);
+
+    two()
+}
+
+//one()
+if(true){
+    const name = "krish"
+
+    if(name == "krish"){
+        const site = "youtube"
+        // console.log(name +" " +  site);
+    }
+    //console.log(site);
+}
+// console.log(name);
+
+// ******************************************************************
+
+console.log(addOne(5));
+function addOne (num){
+    return num + 1
+}
+
+
+// mini hoisting
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(7);
+
